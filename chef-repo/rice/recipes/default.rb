d79cff3584e9c5950491b6754f57032d9d9ec481
Martin Taylor <mztaylor@gmail.com>
2012-11-16 18:57:42 -0500

    updating default.rb file

--- chef-repo/rice/recipes/default.rb
+++ chef-repo/rice/recipes/default.rb
@@ -54,10 +54,20 @@ template "#{node[:rice][:config_dir]}/sample-app-config.xml" do
   owner node[:rice][:user]
 end
 
+bash "symlink_java_home" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    cd /usr/lib/jvm
+    ln -fs java-6-openjdk-i386 default-java
+  EOH
+  action :run
+end
+
+
 ENV['MAVEN_OPTS'] = " -Xmx1g -XX:MaxPermSize=256m "
-ENV['HOME'] = "/home/vagrant/"
 bash "impex_rice_database" do
   cwd Chef::Config[:file_cache_path]
+  usr node[:rice][:user]
   code <<-EOH
     echo cd #{node[:rice][:src_dir]}/rice/db/impex/master
     cd #{node[:rice][:src_dir]}/rice/db/impex/master
@@ -68,6 +78,7 @@ end
 
 bash "compile_rice_source" do
   cwd Chef::Config[:file_cache_path]
+  user node[:rice][:user]
   code <<-EOH
     cd #{node[:rice][:src_dir]}/rice
     mvn -Dmaven.test.skip=true package
@@ -75,19 +86,31 @@ bash "compile_rice_source" do
   action :nothing
 end
 
+bash "install_ojdbc14_jar" do
+  cwd Chef::Config[:file_cache_path]
+  user node[:rice][:user]
+  code <<-EOH
+    cd /tmp/
+    mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc14  -Dversion=10.2.0.3.0 -Dpackaging=jar -Dfile=ojdbc14.jar -DgeneratePom=true
+  EOH
+  action :nothing
+end
+
+cookbook_file "/tmp/ojdbc14.jar" do
+  source "ojdbc14.jar"
+  owner node[:rice][:user]
+  backup false
+  mode "0777"
+  action :create_if_missing
+  notifies :run, resources(:bash => "install_ojdbc14_jar"), :immediately
+end
+
 subversion "rice_source" do
   repository "https://svn.kuali.org/repos/rice/trunk/"
   revision "HEAD"
   destination "/opt/src/rice"
   action :sync
-  notifies resources(:bash => "impex_rice_database"), resources(:bash => "compile_rice_source")
-end
-
-cookbook_file "#{node[:rice][:src_dir]}/rice/pom.xml.patch" do
-  source "rice-pom.xml.patch" # this is the value that would be inferred from the path parameter
-  owner node[:rice][:user]
-  backup false
-  mode "0777"
+  notifies :run, [resources(:bash => "impex_rice_database"), resources(:bash => "compile_rice_source")], :delayed
 end
 
 bash "start_rice_sampleapp" do
@@ -98,6 +121,7 @@ bash "start_rice_sampleapp" do
     cd #{node[:rice][:src_dir]}/rice/sampleapp
     mvn -Dalt.config.location=#{node[:rice][:home_dir]}/sample-app-config.xml -Dmaven.test.skip=true tomcat:run-war -Dojdbc14.jar.location=""
   EOH
+  action :nothing
 end
 
 
